726a496ce066073c28475b6638e1b52d032ed091
sh <intoyrsofat@gmail.com>
2023-02-26 21:39:08 -0500

    Began data storage reading

--- src/cmds/startRaffle.js
+++ src/cmds/startRaffle.js
@@ -1,3 +1,12 @@
+const { promises: fs } = require('fs');
+
+const userdataPath = 'data/userdata';
+
 module.exports.cmd = async (msg) => {
+    let data = await fs.readFile(userdataPath);
+
+    // Add parse data into user structure
+    // { name, entries, status }
+
     await msg.reply('Started Raffle');
 }
