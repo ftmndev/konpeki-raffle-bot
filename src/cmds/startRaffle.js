2b2c465ff179404ef27ec0a2e654652c21aa625f
Borplex <bmcdonald899@gmail.com>
2023-03-06 14:00:04 -0500

    addded jank button

--- src/cmds/startRaffle.js
+++ src/cmds/startRaffle.js
@@ -1,5 +1,5 @@
-const { EmbedBuilder } = require('discord.js');
-const { promises: fs, fs } = require('fs');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, Events } = require('discord.js');
+const { promises: fs } = require('fs');
 const Util = require('../backend/Util');
 
 const userdataPath = 'data/userdata';
@@ -17,30 +17,40 @@ module.exports.cmd = async (msg) => {
     // Add parse data into user structure
     // { name, entries, status }
     
-    const raffleRole = msg.guild.roles.cache.get('1082350587942817802')
+    const raffleRole = msg.guild.roles.cache.get('954586301972217917')
+    const verifiedMember = msg.guild.roles.cache.get('954586301972217917')
 
     const Filter = (reaction, user) => user.id == msg.author.id
 
-    const Embed = new EmbedBuilder()
-        .setDescription(`React here to enter the raffle!`)
+    const reactEmbed = new EmbedBuilder()
+        .setTitle('VOD REVIEW RAFFLE')
+        .setDescription('React here to enter the raffle!')
 
-    const reactionMessage = await msg.channel.send(Embed)
+    const reactButton = new ActionRowBuilder()
+        .addComponents(new ButtonBuilder()
+        .setCustomId('primary')
+        .setLabel('Enter Raffle')
+        .setStyle(ButtonStyle.Primary)
+        .setDisabled(!msg.member.roles.cache.some(role => role.id === '954586301972217917'))
+        )
+
+    const reactionMessage = await msg.channel.send({ embeds: [reactEmbed], components: [reactButton]})
 
     await reactionMessage.react(':white_check_mark:')
 
-    // Time is measured in ms
-    reactionMessage.awaitReactions(Filter, {max: 1, time: 30000, errors: ["time"]}).then(collected => {
+    // // Time is measured in ms
+    // reactionMessage.awaitReactions(Filter, {max: 1, time: 30000, errors: ["time"]}).then(collected => {
        
-        const reaction = collected.first();
+    //     const reaction = collected.first();
         
-        switch (reaction.emoji.name) {
-            case ":white_check_mark:":
-                // Checking if the member already has the role.
-                if (msg.member.roles.cache.has(Role1.id)) {return msg.channel.send("You already have the role.")};
-                // Adding the role.
-                msg.member.roles.add(Role1).then(msg.channel.send("Role added!"));
-                // Breaking the switch statement to make sure no other cases are executed.
-                break
-        }
-    })
+    //     switch (reaction.emoji.name) {
+    //         case ":white_check_mark:":
+    //             // Checking if the member already has the role.
+    //             if (msg.member.roles.cache.has(raffleRole.id)) {return msg.channel.send("You already have the role.")};
+    //             // Adding the role.
+    //             msg.member.roles.add(Role1).then(msg.channel.send("Role added!"));
+    //             // Breaking the switch statement to make sure no other cases are executed.
+    //             break
+    //     }
+    // })
 }
