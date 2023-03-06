abd05a76ce1e7d7b948fa5908ccc6b7147cc7fe0
Borplex <bmcdonald899@gmail.com>
2023-03-06 12:36:54 -0500

    embed start

--- src/cmds/SetPreset.js
+++ src/cmds/SetPreset.js
@@ -1,8 +1,8 @@
-const preset1 = Preset_1;
-const preset2 = Preset_2;
-const preset3 = Preset_3;
-const preset4 = Preset_4;
-const preset5 = Preset_5;
+// const preset1 = Preset_1;
+// const preset2 = Preset_2;
+// const preset3 = Preset_3;
+// const preset4 = Preset_4;
+// const preset5 = Preset_5;
 
 module.exports.cmd = async (msg) => {
     await msg.reply('Set a Preset.');
--- src/cmds/startRaffle.js
+++ src/cmds/startRaffle.js
@@ -1,12 +1,38 @@
+const { SharedNameAndDescription, EmbedBuilder } = require('discord.js');
 const { promises: fs } = require('fs');
 
 const userdataPath = 'data/userdata';
 
 module.exports.cmd = async (msg) => {
-    let data = await fs.readFile(userdataPath);
+    //let data = await fs.readFile(userdataPath);
 
     // Add parse data into user structure
     // { name, entries, status }
+    
+    const raffleRole = msg.guild.roles.cache.get('1082350587942817802')
 
-    await msg.reply('Started Raffle');
+    const Filter = (reaction, user) => user.id == msg.author.id
+
+    const Embed = new EmbedBuilder()
+        .setDescription(`React here to enter the raffle!`)
+
+    const reactionMessage = await msg.channel.send(Embed)
+
+    await reactionMessage.react(':white_check_mark:')
+
+    // Time is measured in ms
+    reactionMessage.awaitReactions(Filter, {max: 1, time: 30000, errors: ["time"]}).then(collected => {
+       
+        const reaction = collected.first();
+        
+        switch (reaction.emoji.name) {
+            case ":white_check_mark:":
+                // Checking if the member already has the role.
+                if (msg.member.roles.cache.has(Role1.id)) {return msg.channel.send("You already have the role.")};
+                // Adding the role.
+                msg.member.roles.add(Role1).then(msg.channel.send("Role added!"));
+                // Breaking the switch statement to make sure no other cases are executed.
+                break
+        }
+    })
 }
