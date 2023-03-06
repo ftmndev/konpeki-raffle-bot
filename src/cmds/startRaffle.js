30662752184550281b61fc17c7f5a08fc16f7083
sh <intoyrsofat@gmail.com>
2023-03-06 14:03:27 -0500

    Removed Comments

--- src/cmds/startRaffle.js
+++ src/cmds/startRaffle.js
@@ -35,22 +35,4 @@ module.exports.cmd = async (msg) => {
         )
 
     const reactionMessage = await msg.channel.send({ embeds: [reactEmbed], components: [reactButton]})
-
-    await reactionMessage.react(':white_check_mark:')
-
-    // // Time is measured in ms
-    // reactionMessage.awaitReactions(Filter, {max: 1, time: 30000, errors: ["time"]}).then(collected => {
-       
-    //     const reaction = collected.first();
-        
-    //     switch (reaction.emoji.name) {
-    //         case ":white_check_mark:":
-    //             // Checking if the member already has the role.
-    //             if (msg.member.roles.cache.has(raffleRole.id)) {return msg.channel.send("You already have the role.")};
-    //             // Adding the role.
-    //             msg.member.roles.add(Role1).then(msg.channel.send("Role added!"));
-    //             // Breaking the switch statement to make sure no other cases are executed.
-    //             break
-    //     }
-    // })
 }
