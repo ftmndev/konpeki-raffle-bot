c2cd0def90cb9e386f67332ee7600614a51109af
Borplex <bmcdonald899@gmail.com>
2023-03-31 13:39:49 -0400

    borplex embed message

--- src/cmds/startRaffle.js
+++ src/cmds/startRaffle.js
@@ -25,14 +25,26 @@ module.exports.cmd = async (msg, client) => {
     // Creates Raffle Role
     module.exports.raffleRole = await msg.guild.roles.create({ name: "Raffle", reason: "Creating Raffle Role" });
     
-    var raffleName = msg.options.getString('raffle-name') ?? 'PEKIRAFFLE';
+    var raffleName = msg.options.getString('raffle-name') ?? 'Vod Review Raffle';
     var raffleDesc = msg.options.getString('raffle-desc') ?? 'Enter by clicking the button below!';
     var coachName = msg.options.getString('coach-name');
 
     // The Raffle Annoucement Embed
     var reactEmbed = new EmbedBuilder()
         .setTitle(raffleName)
-        .setDescription(raffleDesc);
+        .setColor(0x00FFFF)
+        .setURL('https://discord.gg/fX2GSskgEC')
+        .setDescription(raffleDesc)
+        .addFields(
+            { name: 'Role Entries', value: '<@&930277398455402506> - 5\n<@&860451916743573565> -1' },
+            { name: '\u200B', value: '\u200B' },
+            { name: `Coach: ${coachName}`, value: 'Konpeki', inline: true },
+            { name: 'Inline field title', value: 'Some value here', inline: true },
+        )
+        .addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
+        .setImage('https://i.imgur.com/AfFp7pu.png')
+        .setTimestamp()
+        .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
 
     // Raffle Annoucement Embed Button
     var reactButton = new ActionRowBuilder()
